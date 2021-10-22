ea7cc2393bcf771b086178345b8fe20f6fd507e6
Jose Melero <jmelero08@gmail.com>
2021-10-22 12:49:33 -0600

    added to routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { getCurrentUser } from "./actions/currentUser.js"
+import NavBar from "./components/NavBar.js"
+import Home from "./components/Home.js"
+import Login from './components/Login.js'
+import Signup from './components/SignUp.js'
+import MyWorkouts from './components/MyWorkouts.js'
+import WorkoutCard from './components/RestrauantCard';
+import NewWorkoutFormWrapper from './components/NewWorkoutFormWrapper'
+import EditWorkoutFormWrapper from './components/EditWorkoutFormWrapper'
+import { Route, Switch, withRouter } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
+
+  render(){
+    const { loggedIn, workouts } = this.props
+    return (
+      <div className="App">
+        { loggedIn ? <NavBar/> : <Home/> } 
+        <Switch>
+          <Route exact path='/signup' render={({history})=><Signup history={history}/>}/>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/workouts' component={MyWorkouts}/>
+          <Route exact path='/workouts/new' component={NewWorkoutFormWrapper}/>
+          <Route exact path='/workouts/:id' render={props => {
+            const workout = workouts.find(workout => workout.id === props.match.params.id)
+            console.log(workout)
+            return <WorkoutCard workout={workout} {...props}/>
+            }
+          }/>
+          <Route exact path='/workouts/:id/edit' render={props => {
+            const workout = workouts.find(workout => workout.id === props.match.params.id)
+            return <EditWorkoutFormWrapper workout={workout} {...props}/>
+            }
+          }/>
+          </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser,
+    workouts: state.myWorkouts
+  })
+}
+
+
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose, combineReducers }  from 'redux'
+import thunk  from 'redux-thunk'
+import currentUser from './reducers/currentUser.js'
+import loginForm from './reducers/loginForm.js'
+import myWorkouts from './reducers/myWorkouts.js'
+import signupForm from './reducers/signupForm.js'
+import workouttForm from './reducers/WorkouttForm.js'
+
+const reducer = combineReducers({
+    currentUser,
+    loginForm,
+    myWorkouts,
+    signupForm,
+    workouttForm
+  })
+  
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
