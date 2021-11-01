f6fa5d3894dcbea5484e513c3ea536194eae0afe
Jose Melero <jmelero08@gmail.com>
2021-10-31 18:16:44 -0600

    added some styling

--- src/components/MyWorkouts.js
+++ src/components/MyWorkouts.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const MyWorkouts = props => {
     const workoutCards = props.workouts.length > 0 ?
-     props.workouts.map(r => (<p key={r.id}><Link to={`/workouts/${r.id}`}>{r.attributes.name}</Link></p>)) : null
+     props.workouts.map(r => (<p key={r.id}><Link to={`/workouts/${r.id}`} className="MyWorkouts">{r.attributes.name}</Link></p>)) : null
      
     return workoutCards
     
