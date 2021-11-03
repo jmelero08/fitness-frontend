169bbcd0f55f3f2ce6cca4e8f9f4cabef185e017
Jose Melero <jmelero08@gmail.com>
2021-11-02 19:31:31 -0600

    edited home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 
-const Home = ({}) => (
+const Home = () => (
     <div>
         <span>
             <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link>
